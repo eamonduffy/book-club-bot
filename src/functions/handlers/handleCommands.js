cd2f8a24fc450e83e6ddad68c269afda06c9266d
eamonduffy <eamon.duffy0@gmail.com>
2023-02-26 17:15:58 -0500

    base

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,41 @@
+const { REST } = require("@Discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
+        console.log(
+          `Command: ${command.data.name} has been passed through the handler`
+        );
+      }
+    }
+
+    const clientId = "1079497991200718989";
+    const guildId = "890946950759874631";
+
+    const rest = new REST({ version: "9" }).setToken(process.env.token);
+
+    try {
+      console.log("Started refreshing application (/) commands.");
+
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: client.commandArray,
+      });
+
+      console.log("Successfully reloaded application (/) commands.");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
